b07fa79ff63854d7ca2674f68dbadc92e7b0f558
Shaina-Z <113309998+Shaina-Z@users.noreply.github.com>
2022-11-05 17:06:39 -0400

    Add files via upload

---
+++ GreenBallCollisionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    
+    public GameObject greenBall;
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // GreenBall Increases in Size
+        greenBall.transform.localScale += new Vector3(2f, 2f, 2f);
+
+    }
+}
